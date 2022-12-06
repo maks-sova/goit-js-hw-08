e57219d8e5c60cb9a836c9bcdf49f9833bf6a398
maks-sova <maxxxhill2k@gmail.com>
2022-12-07 00:14:43 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
 
+
+const throttle = require('lodash.throttle');
+
+const email = document.querySelector('[type="email"]');
+const message = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+
+ 
+const checkLocalStorage = () => {
+const data = localStorage.getItem("feedback-form-state");
+const parsedData = JSON.parse(data); 
+if (parsedData.email) {
+    
+        email.value = parsedData.email;
+};
+    
+if (parsedData.message) {
+        message.value = parsedData.message;
+    } 
+}
+
+const object = {}
+
+const fillForms = (event) => {
+    
+    object.email = event.currentTarget.elements.email.value; 
+    object.message = event.currentTarget.elements.message.value;    
+   localStorage.setItem("feedback-form-state", JSON.stringify(object)); 
+}
+
+
+const submitForm = (event) => {
+    event.preventDefault();
+    console.log(object);
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    
+}
+
+
+form.addEventListener('input', throttle(fillForms,500));
+form.addEventListener('submit', submitForm);
+document.addEventListener('DOMContentLoaded', checkLocalStorage);
