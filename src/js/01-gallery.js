831ea27ed8bd84b38061a1ee540861fb63903a47
maks-sova <maxxxhill2k@gmail.com>
2022-12-06 23:32:13 +0200

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector('.gallery');
+
+
+
+const newGallery = galleryItems.map(({ preview, original, description}) => 
+    `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} title="подпись"/>
+</a>`)
+    .join("");
+
+gallery.insertAdjacentHTML('afterbegin', newGallery);
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+};
+
+    player.on('timeupdate', throttle(onPlay,1000))
+    
+if (localStorage.getItem("videoplayer-current-time")) {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+    }
